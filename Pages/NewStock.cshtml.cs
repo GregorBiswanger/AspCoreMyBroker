184684009a9b036f6f0e50f414ca3d7a0b2a1b0e
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2018-06-10 19:33:47 +0200

    Two pages with navigation

---
+++ Pages/NewStock.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyBroker.Pages
+{
+    public class NewStockModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
